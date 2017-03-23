61a0acd08a9a86cbb54272f1edd2a12116714c5f
frodrigues <frodrigues@amtera.com.br>
2017-03-23 07:19:08 -0300

    --update react and react-dom version and adding more components.

--- src/js/components/layout/layout.component.js
+++ src/js/components/layout/layout.component.js
@@ -4,13 +4,24 @@ import React from "react";
 import Header from "../header/header.component";
 import Footer from "../footer/footer.component";
 import Content from "../content/content.component";
+import Navbar from "../navbar/navbar.component";
 
 export default class Layout extends React.Component {
+
+	constructor(){
+		super();
+		this.state = {title: "React Tutorial"};
+	}
+
+	changeTitle(title){
+		this.setState({title});
+	}
 
 	render(){
 		return (
 			<div>
-				<Header title="Main Page"/>
+
+				<Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
 				<Content />
 				<Footer />
 			</div>
